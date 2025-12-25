05485b9d1d46b9346a941f65985e55aca91e857b
haikoj <193362097+haikoj@users.noreply.github.com>
2025-12-25 22:40:24 +0200

    Tasks 1.1, 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {part: 'Fundamentals of React', exercises: 10 },
+    {part: 'Using props to pass data', exercises: 7 },
+    {part: 'State of a component', exercises: 14 },
+  ]
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,16 @@
+import Part from "./Part"
+
+
+const Content = (props) => {
+
+    return (
+        <div>
+            <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+            <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+            <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
+        </div>
+
+    )
+}
+
+export default Content
