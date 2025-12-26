8a3646edd548b5378cb14872c2c06ffad18c5da6
haikoj <193362097+haikoj@users.noreply.github.com>
2025-12-26 15:39:22 +0200

    Task 1.5

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -5,9 +5,9 @@ const Content = (props) => {
 
     return (
         <div>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+            <Part name={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
+            <Part name={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+            <Part name={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
         </div>
 
     )
